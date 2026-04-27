cf04b69837ec5b8053f875702e4734572d8c9db2
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-04-27 15:00:29 -0300

    feat: initial elements for Home and Board

--- frontend/src/routes/Board.jsx
+++ frontend/src/routes/Board.jsx
@@ -1,7 +1,66 @@
 const Board = () => {
   return (
     <div className="board">
-      <h1>Board</h1>
+      <div className="filter-bar">
+        <input type="text" name="search" placeholder="Procure uma task" />
+        <button>Filter</button>
+        <button>Create Category</button>
+      </div>
+      <div className="categories">
+        <div className="category">
+          <h3>Backlog</h3>
+          <div className="tasks">
+            <div className="task">
+              <p>Nome da Task</p>
+            </div>
+
+            <div className="task">
+              <p>Nome da Task</p>
+            </div>
+
+            <div className="task">
+              <p>Nome da Task</p>
+            </div>
+          </div>
+          <button>Add Task</button>
+        </div>
+
+        <div className="category">
+          <h3>In Progress</h3>
+          <div className="tasks">
+            <div className="task">
+              <p>Nome da Task</p>
+            </div>
+
+            <div className="task">
+              <p>Nome da Task</p>
+            </div>
+
+            <div className="task">
+              <p>Nome da Task</p>
+            </div>
+          </div>
+          <button>Add Task</button>
+        </div>
+
+        <div className="category">
+          <h3>Finished</h3>
+          <div className="tasks">
+            <div className="task">
+              <p>Nome da Task</p>
+            </div>
+
+            <div className="task">
+              <p>Nome da Task</p>
+            </div>
+
+            <div className="task">
+              <p>Nome da Task</p>
+            </div>
+          </div>
+          <button>Add Task</button>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,7 +1,35 @@
 const Home = () => {
   return (
     <div className="home">
-      <h1>Home</h1>
+      <div className="workpace">
+        <h2>Workpaces 1</h2>
+        <div className="boards">
+          <div className="board">
+            <h3>Primeiro Board</h3>
+            <button>Ver Board</button>
+          </div>
+          <div className="board">
+            <h3>Segundo Board</h3>
+            <button>Ver Board</button>
+          </div>
+          <button className="new-board-btn">Create new Board</button>
+        </div>
+      </div>
+
+      <div className="workpace">
+        <h2>Workpaces 2</h2>
+        <div className="boards">
+          <div className="board">
+            <h3>Primeiro Board</h3>
+            <button>Ver Board</button>
+          </div>
+          <div className="board">
+            <h3>Segundo Board</h3>
+            <button>Ver Board</button>
+          </div>
+          <button className="new-board-btn">Create new Board</button>
+        </div>
+      </div>
     </div>
   );
 };
