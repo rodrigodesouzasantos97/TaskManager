7083f3520228a6c88eecec410c1c12ecf2ff303d
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-04-28 13:58:05 -0300

    feat: create new category button in Board and filter bar in Home

--- frontend/src/routes/Board.jsx
+++ frontend/src/routes/Board.jsx
@@ -6,18 +6,19 @@ const Board = () => {
   return (
     <div className="board">
       <div className="filter-bar">
-        <input type="text" name="search" placeholder="Procure uma task" />
+        <input type="text" name="search" placeholder="Procurar tarefa" />
         <button>
           <i class="fa-solid fa-filter"></i>
         </button>
         <button>
-          <i class="fa-solid fa-plus"></i>Create Category
+          <i class="fa-solid fa-plus"></i>Criar categoria
         </button>
       </div>
       <div className="categories">
         <Category title="Backlog" />
         <Category title="In Progress" />
         <Category title="Done" />
+        <button><i class="fa-solid fa-plus"></i>Criar categoria</button>
       </div>
     </div>
   );
--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,12 +1,18 @@
-import "./Home.css"
+import "./Home.css";
 
 import Workpace from "../components/Workpace";
 
 const Home = () => {
   return (
     <div className="home">
-      <Workpace title="Workpace 1" />
-      <Workpace title="Workpace 2" />
+      <div className="filter-bar">
+        <input type="text" name="search" placeholder="Procurar espaço de trabalho" />
+        <button><i class="fa-solid fa-plus"></i>Criar novo espaço</button>
+      </div>
+      <div className="Workpaces">
+        <Workpace title="Workpace 1" />
+        <Workpace title="Workpace 2" />
+      </div>
     </div>
   );
 };
