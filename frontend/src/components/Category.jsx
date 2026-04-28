181cc32b6745153580352b9148e27871818671a3
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-04-28 16:56:47 -0300

    feat: create functions and states
    - Create createCategory in Board route
    - Create search value state
    - Create filteredTasks in Category component
    - Create createTask function in Category component

--- frontend/src/components/Category.jsx
+++ frontend/src/components/Category.jsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
+
 import Task from "./Task";
 
-const Category = ({ title }) => {
+const Category = ({ title, searchValue }) => {
+  const [tasks, setTasks] = useState([]);
+
+  const createTask = () => {
+    const newTask = {
+      id: Date.now(),
+      description: `Nova tarefa ${Date.now()}`,
+    };
+
+    setTasks([...tasks, newTask]);
+  };
+
+  const filteredTasks = tasks.filter((task) =>
+    task.description.includes(searchValue || "")
+  );
+
   return (
     <div className="category">
       <h3>{title}</h3>
       <div className="tasks">
-        <Task description="Tarefa 1" />
-        <Task description="Tarefa 2" />
-        <Task description="Tarefa 3" />
+        {filteredTasks.map((task) => (
+          <Task key={task.id} description={task.description} />
+        ))}
       </div>
-      <button>
+      <button onClick={createTask}>
         <i className="fa-solid fa-plus"></i>Adicionar tarefa
       </button>
     </div>
--- frontend/src/routes/Board.jsx
+++ frontend/src/routes/Board.jsx
@@ -1,24 +1,46 @@
+import { useState } from "react";
+
 import "./Board.css";
 
 import Category from "../components/Category";
 
 const Board = () => {
+  const [categories, setCategories] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  const createCategory = () => {
+    const newCategory = {
+      id: Date.now(),
+      title: "Nova categoria",
+    };
+
+    setCategories([...categories, newCategory]);
+  };
+
   return (
     <div className="board">
       <div className="filter-bar">
-        <input type="text" name="search" placeholder="Procurar tarefa" />
+        <input
+          type="text"
+          name="search"
+          placeholder="Procurar tarefa"
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
         <button>
           <i className="fa-solid fa-filter"></i>
         </button>
-        <button>
+        <button onClick={createCategory}>
           <i className="fa-solid fa-plus"></i>Criar categoria
         </button>
       </div>
       <div className="categories">
-        <Category title="Backlog" />
-        <Category title="In Progress" />
-        <Category title="Done" />
-        <button><i className="fa-solid fa-plus"></i>Criar categoria</button>
+        {categories.map((category) => (
+          <Category
+            key={category.id}
+            title={category.title}
+            searchValue={searchValue}
+          />
+        ))}
       </div>
     </div>
   );
