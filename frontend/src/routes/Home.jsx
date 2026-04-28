f27b80c46f06e452bc02e68fe1e19feebbab089f
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-04-28 16:50:36 -0300

    feat: create createWorkpace and searchWorkpaces functions in Home route

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,17 +1,61 @@
+import { useState } from "react";
+
 import "./Home.css";
 
 import Workpace from "../components/Workpace";
 
 const Home = () => {
+  const [workpaces, setWorkpaces] = useState([]);
+  const [filteredWorkpaces, setFilteredWorkpaces] = useState([]);
+
+  const createWorkpace = () => {
+    const newWorkpace = {
+      id: Date.now(),
+      title: `Novo espaço de trabalho ${Date.now()}`,
+    };
+
+    const updated = [...workpaces, newWorkpace];
+
+    setWorkpaces(updated);
+    setFilteredWorkpaces(updated);
+  };
+
+  const searchWorkpaces = (value) => {
+    if (!value) {
+      setFilteredWorkpaces(workpaces);
+      return;
+    }
+
+    const filtered = workpaces.filter((workpace) =>
+      workpace.title.includes(value),
+    );
+
+    setFilteredWorkpaces(filtered);
+  };
+
   return (
     <div className="home">
       <div className="filter-bar">
-        <input type="text" name="search" placeholder="Procurar espaço de trabalho" />
-        <button><i className="fa-solid fa-plus"></i>Criar novo espaço</button>
+        <input
+          type="text"
+          name="search"
+          placeholder="Procurar espaço de trabalho"
+          onChange={(e) => searchWorkpaces(e.target.value)}
+        />
+        <button onClick={createWorkpace}>
+          <i className="fa-solid fa-plus"></i>Criar novo espaço
+        </button>
       </div>
       <div className="Workpaces">
-        <Workpace title="Workpace 1" />
-        <Workpace title="Workpace 2" />
+        {filteredWorkpaces.length === 0 && (
+          <p>
+            Você ainda não tem nenhum espaço de trabalho
+            <i className="fa-solid fa-face-frown"></i>
+          </p>
+        )}
+        {filteredWorkpaces.map((workpace) => (
+          <Workpace key={workpace.id} title={workpace.title} />
+        ))}
       </div>
     </div>
   );
