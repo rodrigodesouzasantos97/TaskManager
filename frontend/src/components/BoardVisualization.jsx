faa85650e572ff37e0038921c423c9a7b77be4a9
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-04-29 23:59:18 -0300

    refactor: change component name (Board to BoardVisualization)

---
+++ frontend/src/components/BoardVisualization.jsx
@@ -0,0 +1,10 @@
+const BoardVisualization = ({ title }) => {
+  return (
+    <div className="Board-visualization">
+      <h3>{title}</h3>
+      <button>Ver quadro</button>
+    </div>
+  );
+};
+
+export default BoardVisualization;
