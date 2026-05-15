7ac37e60adb3f1863fb74acb160effbd917f36c9
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-05-15 17:50:58 -0300

    feat: create app script to initialize express and cors

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const port = 3000;
+
+app.listen(port, function() {
+    console.log(`Servidor iniciou na porta: ${port}`)
+});
